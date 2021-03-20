38ce5ab53f0bf1547b346532dd27f047fe84c59d
Rudraksh Jhalani <beingrudraksh.07@gmail.com>
2021-03-20 08:38:24 +0530

    Application ready

--- app/src/main/java/com/rudraksh/weather/MainActivity.java
+++ app/src/main/java/com/rudraksh/weather/MainActivity.java
@@ -1,15 +1,14 @@
 package com.rudraksh.weather;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,38 +19,29 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView backgroundImage;
-    private Button search,weather;
-    private EditText cityName;
-    private TextView label,data;
+    Button weather;
+    EditText cityName;
+    TextView label,data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        backgroundImage=findViewById(R.id.backgroundImage);
-        search=findViewById(R.id.searchCityButton);
         weather=findViewById(R.id.showWeatherButton);
         cityName=findViewById(R.id.cityNameInput);
         label=findViewById(R.id.labelCity);
         data=findViewById(R.id.labelWeatherData);
-    }
 
-    public void openSearch(View view) {
-
-        search.setVisibility(View.GONE);
         label.setText("");
         data.setText("");
         cityName.setText("");
         cityName.setVisibility(View.VISIBLE);
         weather.setVisibility(View.VISIBLE);
-        backgroundImage.animate().alpha(0.6f).setDuration(1000);
     }
 
     public void showWeather(View view) {
@@ -59,13 +49,16 @@ public class MainActivity extends AppCompatActivity {
         cityName.setVisibility(View.GONE);
         weather.setVisibility(View.GONE);
 
-        new downloadWeatherData().execute("https://openweathermap.org/data/2.5/weather?q="+cityName.getText().toString()+"&appid=b6907d289e10d714a6e88b30761fae22");
+        new downloadWeatherData().execute("https://api.openweathermap.org/data/2.5/weather?q="
+                +cityName.getText().toString()
+                +"&appid=d5acd5fd438d283e496dbb2b249d8cb6");
 
         //FOR HIDING THE KEYBOARD AS THE BUTTON IS PRESSED AFTER ENTERING THE TEXT
         InputMethodManager manager= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         manager.hideSoftInputFromWindow(cityName.getWindowToken(),0);
     }
 
+    @SuppressLint("StaticFieldLeak")
     class downloadWeatherData extends AsyncTask<String,Void,String >
     {
 
@@ -78,45 +71,28 @@ public class MainActivity extends AppCompatActivity {
             try {
                 url=new URL(strings[0]);
 
-                urlConnection= (HttpURLConnection) url.openConnection();
+                urlConnection = (HttpURLConnection) url.openConnection();
 
-                InputStreamReader reader=new InputStreamReader(urlConnection.getInputStream());
+                InputStreamReader reader = new InputStreamReader(urlConnection.getInputStream());
 
-                int weatherData=reader.read();
+                int weatherData = reader.read();
 
-                String weatherInfo="";
-                while (weatherData!=-1)
+                String weatherInfo = "";
+                while (weatherData != -1)
                 {
-                    weatherInfo+=(char)weatherData;
-                    weatherData=reader.read();
+                    weatherInfo += (char)weatherData;
+                    weatherData = reader.read();
                 }
 
                 return weatherInfo;
-
-            } catch (MalformedURLException e) {
-
-                e.printStackTrace();
-
-                search.setText("Check weather again");
-                search.setVisibility(View.VISIBLE);
-
-                Toast.makeText(getApplicationContext(),"Oops! Could not get the weather.\nMake sure you enter the city name and try again",Toast.LENGTH_SHORT).show();
-
-                return null;
 
             } catch (IOException e) {
 
                 e.printStackTrace();
 
-                search.setText("Check weather again");
-                search.setVisibility(View.VISIBLE);
-
-                Toast.makeText(getApplicationContext(),"Oops! Could not get the weather.\nMake sure you enter the city name and try again",Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(getApplicationContext(),R.string.error_msg,Toast.LENGTH_SHORT).show();
                 return null;
             }
-
-
         }
 
         @Override
@@ -125,9 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
             try {
 
-                Log.i("Weather",s);
-
-                JSONObject jsonObject=new JSONObject(s);
+                JSONObject jsonObject = new JSONObject(s);
 
                 /** IF WE CHECK THE WEATHER JSON RECEIVED, IT HAS A SQUARE BRACKET BEFORE THE CURLY ONES IN "weather" COLUMN
                  * THAT'S WHY WE FIRST USE JSONArray FOR IT TO STORE ALL THE DATA AND THEN RETRIEVE IT BY STORING IN JSONObject ONE BY ONE
@@ -135,11 +109,11 @@ public class MainActivity extends AppCompatActivity {
                  * HENCE, WE ACCESS THEM DIRECTLY THROUGH THE JSONObject
                  * THE THREE COLUMNS IN LAST (id,name,cod) ARE ACCESSED DIRECTLY BY THE MAIN JSONObject AS THEY ARE PART OF THE MAIN JSON DATA  */
 
-                String WEATHER="",TITLE="";
+                String WEATHER = "",TITLE = "";
 
                 /** GETTING WEATHER DESCRIPTION  */
 
-                JSONArray jsonArray=new JSONArray(jsonObject.getString("weather"));
+                JSONArray jsonArray = new JSONArray(jsonObject.getString("weather"));
 
                 for (int i=0;i<jsonArray.length();i++)
                 {
@@ -190,16 +164,10 @@ public class MainActivity extends AppCompatActivity {
                 label.setVisibility(View.VISIBLE);
                 data.setVisibility(View.VISIBLE);
 
-                search.setText("Check weather again");
-                search.setVisibility(View.VISIBLE);
-
             } catch (JSONException e) {
                 e.printStackTrace();
 
-                search.setText("Check weather again");
-                search.setVisibility(View.VISIBLE);
-
-                Toast.makeText(getApplicationContext(),"Oops! Could not get the weather.\nMake sure you enter the city name and try again",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.error_msg,Toast.LENGTH_SHORT).show();
             }
         }
     }
